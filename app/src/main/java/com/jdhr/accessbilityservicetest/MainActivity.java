b617a2d839e6df8a5da99a3879749fefdfc6f75b
yinqi <251282197.com>
2018-09-13 11:36:27 +0800

    完善处理逻辑

--- app/src/main/java/com/jdhr/accessbilityservicetest/MainActivity.java
+++ app/src/main/java/com/jdhr/accessbilityservicetest/MainActivity.java
@@ -1,20 +1,45 @@
 package com.jdhr.accessbilityservicetest;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvStartAccessibilityService;
-
-    private void assignViews() {
-        tvStartAccessibilityService = (TextView) findViewById(R.id.tv_startAccessibilityService);
-    }
+    private TextView tvInstall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
+        tvStartAccessibilityService.setOnClickListener(v -> {
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
+        tvInstall.setOnClickListener(v -> autoInstall());
+    }
+
+    private void assignViews() {
+        tvStartAccessibilityService = (TextView) findViewById(R.id.tv_startAccessibilityService);
+        tvInstall = findViewById(R.id.tv_install);
+    }
+
+    public void autoInstall() {
+        //sd 卡路径下放入名为 test 的安装包
+        String apkPath = Environment.getExternalStorageDirectory() + "/test.apk";
+        Uri uri = Uri.fromFile(new File(apkPath));
+        Intent localIntent = new Intent(Intent.ACTION_VIEW);
+        localIntent.setDataAndType(uri, "application/vnd.android.package-archive");
+        startActivity(localIntent);
     }
 }
--- app/src/main/java/com/jdhr/accessbilityservicetest/MyAccessibilityService.java
+++ app/src/main/java/com/jdhr/accessbilityservicetest/MyAccessibilityService.java
@@ -1,6 +1,8 @@
 package com.jdhr.accessbilityservicetest;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -10,13 +12,23 @@ import java.util.Map;
 
 public class MyAccessibilityService extends AccessibilityService {
 
-    private static final String TAG = "[TAG]";
+    private static final String TAG = "MyAccessibilityService";
     private Map<Integer, Boolean> handleMap = new HashMap<>();
+
+    @Override
+    protected void onServiceConnected() {
+        AccessibilityServiceInfo serviceInfo = getServiceInfo();
+        //指定监听包名
+        serviceInfo.packageNames = new String[]{"com.android.packageinstaller", "com.jdhr.accessbilityservicetest"};
+        setServiceInfo(serviceInfo);
+        super.onServiceConnected();
+    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
 
-        AccessibilityNodeInfo nodeInfo = event.getSource();
+        //用getSource无法监听到安装完成的界面，改用 getRootInActiveWindow
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo != null) {
             int eventType = event.getEventType();
             if (eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED || eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
@@ -35,14 +47,26 @@ public class MyAccessibilityService extends AccessibilityService {
     private boolean iterateNodesAndHandle(AccessibilityNodeInfo nodeInfo) {
         if (nodeInfo != null) {
             int childCount = nodeInfo.getChildCount();
-            if ("android.widget.Button".equals(nodeInfo.getClassName())) {
-                String nodeCotent = nodeInfo.getText().toString();
-                Log.d(TAG, "content is: " + nodeCotent);
-                if ("安装".equals(nodeCotent) || "完成".equals(nodeCotent) || "确定".equals(nodeCotent)) {
+            String nodeContent = null;
+            if (null != nodeInfo.getText()) {
+                nodeContent = nodeInfo.getText().toString();
+                Log.i(TAG, "iterateNodesAndHandle: " + nodeContent);
+            }
+            if (nodeInfo.isCheckable()) {//华为，勾选同意安装
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            } else if (!TextUtils.isEmpty(nodeContent) && (nodeContent.contains("安装") || nodeContent.contains("确定"))) {
+                if (nodeInfo.isClickable()) {
                     nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                     return true;
                 }
+            } else if (!TextUtils.isEmpty(nodeContent) && nodeContent.contains("打开")) {//安装完成后打开
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                return true;
             }
+//            else if (!TextUtils.isEmpty(nodeContent) && nodeContent.equals("删除安装包")) {
+//                Log.i(TAG, "iterateNodesAndHandle:delete " + nodeInfo.getChild(2).getText().toString());
+//                nodeInfo.getChild(2).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+//            }
             //遇到ScrollView的时候模拟滑动一下
             else if ("android.widget.ScrollView".equals(nodeInfo.getClassName())) {
                 nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
@@ -55,6 +79,19 @@ public class MyAccessibilityService extends AccessibilityService {
             }
         }
         return false;
+    }
+
+    public void performViewClick(AccessibilityNodeInfo nodeInfo) {
+        if (nodeInfo == null) {
+            return;
+        }
+        while (nodeInfo != null) {
+            if (nodeInfo.isCheckable()) {
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                break;
+            }
+            nodeInfo = nodeInfo.getParent();
+        }
     }
 
     @Override
